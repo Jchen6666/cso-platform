61e679c818a53e187d7c9a4a5b39bd37f68a58d4
Jerry Chen <cj154014737@hotmail.com>
2019-12-22 23:21:14 +0100

    Finished the algorithm

--- src/Patient.js
+++ src/Patient.js
@@ -1,28 +1,61 @@
-class Patient{
-static countries = {
+let countries = {
     UK: "UK",
     US: "US",
     DK: "DK"
 };
-static conditions = {
+let conditions = {
     SPORADIC: "sporadic",
     CHRONICAL: "chronical"
 }
-static drugs = {
+let drugs = {
     1: "Medicine 1",
     2: "Medicine 2",
     3: "Medicine 3"
-}
-diagnose = 0;
-constructor(country){
-    switch(country) { 
-        case Patient.countries.DK:
-            this.country = country
-        case Patient.countries.UK:
-            this.country = country
-        case Patient.countries.US:
-            this.country = country
+} 
+
+function detectPatientEligible(country, diagnose, condition, previousMedicine) {
+    switch (country) {
+        case countries.UK:
+            if(condition == conditions.SPORADIC) {
+                return (previousMedicine == 2 || previousMedicine == drugs[2]) ||
+                    ((previousMedicine == 3 || previousMedicine == drugs[3]) && diagnose <= 5)
+            } else if(ondition == conditions.CHRONICAL) {
+                return (previousMedicine == 1 || previousMedicine == drugs[1]) ||
+                    ((previousMedicine == 3 || previousMedicine == drugs[3]) && diagnose > 5)
+            }
+        case countries.US:
+            if(condition == conditions.SPORADIC) {
+                return !((previousMedicine == 3 || previousMedicine == drugs[3]) && diagnose <= 5)
+            } else if (condition == conditions.CHRONICAL) {
+                return ((previousMedicine == 2 || previousMedicine == drugs[2]) && diagnose <= 5) ||
+                    ((previousMedicine == 1 || previousMedicine == drugs[1]) && diagnose > 5)
+            }
+        case countries.DK:
+            if(condition == conditions.SPORADIC) {
+                return !((previousMedicine == 2 || previousMedicine == drugs[2]) && diagnose > 5)
+            } else if (condition == conditions.CHRONICAL) {
+                return (previousMedicine == 1 || previousMedicine == drugs[1]) ||
+                ((previousMedicine == 3 || previousMedicine == drugs[3]) && diagnose > 5) ||
+                ((previousMedicine == 2 || previousMedicine == drugs[2]) && diagnose <= 5)
+            }
+        default:
+            return false
     }
-  
 }
+
+var isPatientEligible = function(country, diagnose, condition, previousMedicine) {
+    return new Promise(function(resolve, reject) {
+    let result = detectPatientEligible(country, diagnose, condition, previousMedicine)
+    if(result) {
+        resolve(result)
+    } else {
+        reject(false)
+    }
+    })
 }
+
+isPatientEligible("UK", 6, "sporadic", 2).then(function(fromResolve){
+    console.log(fromResolve)
+}).catch(function(fromReject){
+    console.log(fromReject)
+})
